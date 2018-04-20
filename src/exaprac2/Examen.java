9bd867d5482b6eb7a8d0182c5be58f181f20904e
Laboratorio2 <Laboratorio2@172.16.12.116>
2018-04-20 13:30:41 -0600

    examen practico numero 2

--- src/exaprac2/Examen.java
+++ src/exaprac2/Examen.java
@@ -3,5 +3,14 @@ package exaprac2;
 public class Examen {
     public static void main(String[] args) {
         
+        int opcion;
+        
+        
+        
+        if (opcion==1) {
+            
+        }
+        
+        
     }
 }
